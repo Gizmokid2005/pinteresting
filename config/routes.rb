8079d410496cda4b8c5f8ca6b097eae9d7bf1c3e
Gizmokid2005 <gizmokid2005@gmail.com>
2014-01-21 15:25:41 -0500

    Some more customization and a new page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Pinteresting::Application.routes.draw do
   get "pages/home" # Let's keep the routing to keep /pages/home working
   
   get "about" => "pages#about" # About page!! :-) -- creates about_path
-  
+
+  get "signup" => "pages#signup" # Signup page
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
