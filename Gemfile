4e6abaa86b23c114fd808a32d63b75f6971ef089
Gizmokid2005 <gizmokid2005@gmail.com>
2014-01-22 23:52:29 -0500

    Added paperclip-storage-ftp for now in lieu of S3. Full functionality to follow.

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 1.2'          # JSON API builder
 gem 'bootstrap-sass', '>= 3.0.3'  # UI Pretty
 gem 'devise', '~> 3.2.2'          # User management
 gem 'paperclip', '~> 3.2'         # Image funtimes
+gem 'paperclip-storage-ftp'       # Image Storage
 
 group :development, :test do
   gem 'sqlite3'                   # DB
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,18 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  
+  :storage => :ftp,
+  :path => '/public_html/pinteresting/:filename',
+  :url => 'http://img.gizmokid2005.com/pinteresting/:filename',
+  :ftp_servers => [
+    {
+      :host => ENV['FTP_HOST'],
+      :user => ENV['FTP_UN'],
+      :password => ENV['FTP_PW'],
+      :port => 21,
+      :passive => true
+    }
+  ]
 end
