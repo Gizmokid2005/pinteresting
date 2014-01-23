bc3c7858f9681b28766fabe6cc5d00b0e0628d1e
Gizmokid2005 <gizmokid2005@gmail.com>
2014-01-23 09:38:59 -0500

    Now we store pins uniquely

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,8 +4,8 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
   
   :storage => :ftp,
-  :path => '/public_html/pinteresting/:user_id/:style/:filename',
-  :url => 'http://img.gizmokid2005.com/pinteresting/:user_id/:style/:filename',
+  :path => '/public_html/pinteresting/:user_id/:id/:style/:filename',
+  :url => 'http://img.gizmokid2005.com/pinteresting/:user_id/:id/:style/:filename',
   :ftp_servers => [
     {
       :host => ENV['FTP_HOST'],
